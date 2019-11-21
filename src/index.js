313f56279e46955b10a43eec63add717a3540d5f
张金龙 <zhangjinlong@znlhzl.com>
2019-11-21 10:35:29 +0800

    fix: setting 模板配置

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
+
+const App = () => {
+  return <div>Hello React!</div>;
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
